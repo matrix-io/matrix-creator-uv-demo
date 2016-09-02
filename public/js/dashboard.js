7f68810cc62644f8b3bb23232073f0132c870a84
Admobilize <admobilize@iMac-de-Admobilize.local>
2016-09-02 15:41:34 -0500

    add chart uv factor

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,66 +1,102 @@
-
-
 var socket = io();
-var data_uv
-var gauge1
-var config1
-
-
-
 // Flot Line Chart with Tooltips
+
 $(document).ready(function() {
-    console.log("document ready");
-    socket.emit('page_render',{data : '1'});   
+   //Options for chart 
+   var gaugeOptions = {
 
-    config1 = liquidFillGaugeDefaultSettings();
-    config1.circleColor = "#FF7777";
-    config1.textColor = "#FF4444";
-    config1.waveTextColor = "#FFAAAA";
-    config1.waveColor = "#FFDDDD";
-    config1.circleThickness = 0.2;
-    config1.textVertPosition = 0.2;
-    config1.waveAnimateTime = false;
-    config1.maxValue = 20;
-    config1.displayPercent = false;
-    gauge1= loadLiquidFillGauge("fillgauge2", 0 ,config1);
+      chart: {
+         type: 'solidgauge'
+      },
+
+      title: null,
+
+      pane: {
+         center: ['50%', '85%'],
+         size: '140%',
+         startAngle: -90,
+         endAngle: 90,
+         background: {
+            backgroundColor: (Highcharts.theme && Highcharts.theme.background2) || '#EEE',
+            innerRadius: '60%',
+            outerRadius: '100%',
+            shape: 'arc'
+         }
+      },
+
+      tooltip: {
+         enabled: false
+      },
+
+      yAxis: {
+         stops: [
+          [2/17, '#3EA72D'], // green
+          [6/17, '#FFF300'], // yellow
+          [8/17, '#F18B00'], // Orange
+          [11/17, '#E6310F'], // red
+          [12/17, '#B467A3']  // violet
+         ],
+         lineWidth: 0,
+         minorTickInterval: null,
+         tickAmount: 2,
+         title: {
+            y: -70
+         },
+         labels: {
+            y: 16
+         }
+      },
+      plotOptions: {
+         solidgauge: {
+            dataLabels: {
+               y: 5,
+               borderWidth: 0,
+               useHTML: true
+            }
+         }
+      }
+   };
+
+   // The UV chart
+   $('#container-uv').highcharts(Highcharts.merge(gaugeOptions, {
+      yAxis: {
+         min: 0,
+         max: 17,
+         title: {
+            text: 'UV'
+         }
+      },
+
+      credits: {
+         enabled: false
+      },
+
+      series: [{
+         name: 'uv',
+         data: [0],
+         dataLabels: {
+             format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+                 ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
+                    '<span style="font-size:12px;color:silver">UV factor</span></div>'
+         },
+         tooltip: {
+             valueSuffix: ' km/h'
+         }
+     }]
+   }));
 });
 
+/**
+ * listen to the event and update chart
+ * @param  {Object} data UV data 
+ */
 socket.on('update-uv', function(data) {
-  data_uv = data
-  gauge1.update(11)//data.uv_index);
-  updateLiquid(data_uv);
-  console.log(data)
+   var chart = $('#container-uv').highcharts(),
+   point;
+   if (chart) {
+      point = chart.series[0].points[0];
+      point.update(data.uv_index.toFixed(2));
+   }
+   console.log(data)
 });
-
-function updateLiquid(data){
-    if (data.oms_risk ==  "Low" ){
-      config1.circleColor = "#FF7777";
-      config1.textColor = "#FF4444";
-      config1.waveTextColor = "#FFAAAA";
-      config1.waveColor = "#FFDDDD"; 
-    } else if (data == "Moderate") {
-      config1.circleColor = "#FFF300";
-      config1.textColor = "#FFF300";
-      config1.waveTextColor = "#FFF300";
-      config1.waveColor = "#FFF300";
-    } else if (data == "High") {
-      config1.circleColor = "#F18B00";
-      config1.textColor = "#F18B00";
-      config1.waveTextColor = "#F18B00";
-      config1.waveColor = "#F18B00";
-    } else if (data == "Very High") {
-      config1.circleColor = "#E6310F";
-      config1.textColor = "#E6310F";
-      config1.waveTextColor = "#E6310F";
-      config1.waveColor = "#E6310F";
-    } else {
-      config1.circleColor = "#B467A3";
-      config1.textColor = "#B467A3";
-      config1.waveTextColor = "#B467A3";
-      config1.waveColor = "#B467A3";
-    }
-console.log(data.uv_index)
-//gauge1= loadLiquidFillGauge("fillgauge2", 0 , config1);
-
-}
 
